44033b8c53b4f03a509cadf7985a1b277c0a33b5
Jacob Juarez <jacobjuarez1996@gmail.com>
2023-01-24 20:58:00 -0600

    Added Seeded Data
    Added Seeded Data and updated my Connection String

--- SP23.P01.Web/Data/DataContext.cs
+++ SP23.P01.Web/Data/DataContext.cs
@@ -13,6 +13,22 @@ namespace SP23.P01.Web.Data
 
         public DbSet<TrainStation> TrainStations { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+
+            // this stores all decimal values to two decimal points by default - good enough for our purposes
+            configurationBuilder.Properties<decimal>()
+                .HavePrecision(18, 2);
+        }
+
     }
 }
 
---
+++ SP23.P01.Web/Data/SeedData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SP23.P01.Web.Entities;
+
+namespace SP23.P01.Web.Data
+{
+	
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider services)
+        {
+            var context = services.GetRequiredService<DataContext>();
+            context.Database.Migrate();
+
+            AddTrainStations(context);
+        }
+
+        private static void AddTrainStations(DataContext context)
+        {
+            var trainStations = context.Set<TrainStation>();
+            if (trainStations.Any())
+            {
+                return;
+            }
+
+            trainStations.Add(new TrainStation
+            {
+                Name = "Hammond Station",
+                Address = "123 Hammond St"
+            });
+            trainStations.Add(new TrainStation
+            {
+                Name = "Baton Rouge Station",
+                Address = "456 Rouge Ln"
+            });
+            trainStations.Add(new TrainStation
+            {
+                Name = "New Orleans Station",
+                Address = "789 Nola Ave"
+            });
+            context.SaveChanges();
+        }
+    }
+}
+
